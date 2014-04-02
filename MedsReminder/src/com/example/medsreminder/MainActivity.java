4712e3f3f5a100054cb0c09fc3ab2ab32f64c694
angelo102@gmail.com <angelo102@gmail.com@680dcbfc-6249-02fe-906e-2fadfbc6e21d>
2014-04-02 06:39:16 +0000

    Starting alarm scheduling

--- MedsReminder/src/com/example/medsreminder/MainActivity.java
+++ MedsReminder/src/com/example/medsreminder/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.medsreminder;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
@@ -26,8 +29,22 @@ public class MainActivity extends Activity {
 	
 	public void sendMessage(View view){
 		
-		Intent intent = new Intent(this,ListViewActivity.class);
-		startActivity(intent);
+		//Intent intent = new Intent(this,ListViewActivity.class);
+		//startActivity(intent);
+		
+		// get a Calendar object with current time
+		 Calendar cal = Calendar.getInstance();
+		 // add 5 minutes to the calendar object
+		 cal.add(Calendar.SECOND, 5);
+		 Intent intent = new Intent(view.getContext() , MedNotificationReceiver.class);
+		 intent.setAction("com.example.medsreminder.MedNotificationReceiver");
+		 intent.putExtra("alarm_message", "O'Doyle Rules!");
+		 // In reality, you would want to have a static variable for the request code instead of 192837
+		 PendingIntent sender = PendingIntent.getBroadcast(this, 192837, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		 
+		 // Get the AlarmManager service
+		 android.app.AlarmManager am = (android.app.AlarmManager) getSystemService(ALARM_SERVICE);
+		 am.set(android.app.AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), sender);
 	}
 	
 }
